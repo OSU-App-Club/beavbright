4f011934f9130bd9bde23f91c7b4bb58288d36e1
Thomas <nyumat18@gmail.com>
2024-05-03 14:27:37 -0700

    database: add seeding

---
+++ packages/database/prisma/seed/seed.ts
@@ -0,0 +1,31 @@
+/**
+ * ! Executing this script will delete all data in your database and seed it with 10 user.
+ * ! Make sure to adjust the script to your needs.
+ * Use any TypeScript runner to run this script, for example: `npx tsx seed.ts`
+ * Learn more about the Seed Client by following our guide: https://docs.snaplet.dev/seed/getting-started
+ */
+import { createSeedClient } from "@snaplet/seed";
+
+const main = async () => {
+  const seed = await createSeedClient();
+
+  // Truncate all tables in the database
+  await seed.$resetDatabase();
+
+  // Seed the database with 10 user
+  await seed.user((x) => x(10));
+
+  // Seed the database with 10 discussions
+  await seed.discussions((x) => x(10));
+
+  // Seed the database with replies
+  await seed.reply((x) => x(10));
+
+  // Type completion not working? You might want to reload your TypeScript Server to pick up the changes
+
+  console.log("Database seeded successfully!");
+
+  process.exit();
+};
+
+main();
