0ee9c8dea4fd6fc2a4e2f4fc7cf2d67b1e0daef1
Thomas <nyumat18@gmail.com>
2024-05-07 18:38:38 -0700

    web: icon constants and .ts -> .tsx

--- apps/web/components/icons.tsx
+++ apps/web/components/icons.tsx
@@ -1,11 +1,18 @@
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import {
+  BookIcon,
+  BrainCog,
+  BrainIcon,
   EyeIcon,
   EyeOffIcon,
+  HomeIcon,
+  ListIcon,
+  Loader2Icon,
+  LogOutIcon,
   Pyramid,
   Shell,
-  BrainCog,
-  Loader2Icon,
+  User2Icon,
+  Users,
 } from "lucide-react";
 export const Icons = {
   logo: BrainCog,
@@ -13,4 +20,15 @@ export const Icons = {
   spinner: Loader2Icon,
   eye: EyeIcon,
   eyeOff: EyeOffIcon,
+  pyramid: Pyramid,
+  shell: Shell,
+  brainCog: BrainCog,
+  loader2: Loader2Icon,
+  book: BookIcon,
+  brain: BrainIcon,
+  home: HomeIcon,
+  list: ListIcon,
+  logOut: LogOutIcon,
+  user2: User2Icon,
+  users: Users,
 };
