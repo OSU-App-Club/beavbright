78f01096f6b24b21e40fcbc73e87206d3d49520f
tankit03 <huparetanish@gmail.com>
2024-05-23 20:42:36 -0700

    trying out different video sharing

--- apps/web/app/platform/study-groups/session/page.tsx
+++ apps/web/app/platform/study-groups/session/page.tsx
@@ -1,13 +1,10 @@
 import dynamic from 'next/dynamic';
 
-const View = dynamic(() => import('./view'), { ssr: false });
-
 export default function session() {
     return (
         <>
             <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {/* <h1>Welcome to the live sessions !</h1> */}
-                <View />
+                <p>HELLO</p>
             </main>
         </>
     );
