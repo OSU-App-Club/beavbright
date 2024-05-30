a46d4aea5c108f6f477c69220d043992f6bd8efc
owenkrause <owenjkrause@gmail.com>
2024-05-29 19:04:01 -0700

    prisma with auth, run npx prisma client to see your data

--- apps/web/app/auth.ts
+++ apps/web/app/auth.ts
@@ -1,7 +1,9 @@
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
