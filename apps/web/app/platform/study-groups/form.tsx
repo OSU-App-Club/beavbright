758392cd6efb1e0ade0970518db6c0c48d78c2ac
Thomas <nyumat18@gmail.com>
2024-06-03 03:29:06 -0700

    create study group form

--- apps/web/app/platform/study-groups/form.tsx
+++ apps/web/app/platform/study-groups/form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createCourse } from "@/app/lib/actions";
+import { createStudyGroup, createCourse } from "@/app/lib/actions";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import { Button } from "@ui/components/ui/button";
@@ -21,13 +21,21 @@ import {
   FormMessage,
 } from "@ui/components/ui/form";
 import { Input } from "@ui/components/ui/input";
+import MultipleSelector, { Option } from "@ui/components/ui/multi-select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@ui/components/ui/select";
 import { Plus } from "lucide-react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 
-export default function CourseForm() {
+export function JoinCourseForm() {
   const [loading, setLoading] = useState<boolean>(false);
 
   const formSchema = z.object({
@@ -57,14 +65,14 @@ export default function CourseForm() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline" className="h-full text-xl">
+        <Button variant="outline" className="text-primary">
           <Plus className="mr-2" />
           Add Course
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[450px]">
         <DialogHeader>
-          <DialogTitle>Add Course</DialogTitle>
+          <DialogTitle>Create Course</DialogTitle>
           <DialogDescription>Add your course here.</DialogDescription>
         </DialogHeader>
         <div className="grid">
@@ -116,6 +124,149 @@ export default function CourseForm() {
                   </FormItem>
                 )}
               />
+              {loading ? (
+                <Button disabled className="w-full">
+                  <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                  Creating...
+                </Button>
+              ) : (
+                <Button type="submit" className="w-full">
+                  Add
+                </Button>
+              )}
+            </form>
+          </Form>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export function CreateStudyGroupForm({
+  users,
+  courses,
+}: {
+  users: { id: string; name: string | null; image: string | null }[];
+  courses: {
+    id: string;
+    subject: string;
+    code: number;
+    title: string;
+    createdAt: Date;
+  }[];
+}) {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const optionSchema = z.object({
+    label: z.string(),
+    value: z.string(),
+    disable: z.boolean().optional(),
+  });
+
+  const formSchema = z.object({
+    course: z.string().min(1),
+    users: z.array(optionSchema).min(1),
+  });
+
+  const options: Option[] = users.map((user) => ({
+    label: user.name || "Unknown",
+    value: user.id,
+    disable: false,
+  }));
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setLoading(true);
+    try {
+      await createStudyGroup(values);
+      setLoading(false);
+      toast.success("Course created successfully");
+      form.reset();
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      toast.error("Failed to create course");
+    }
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" className="text-primary">
+          <Plus className="mr-2" />
+          Create new study group
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[450px]">
+        <DialogHeader>
+          <DialogTitle>Create Study Group</DialogTitle>
+          <DialogDescription>Fill out the fields.</DialogDescription>
+        </DialogHeader>
+        <div className="grid">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="course"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Course</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={"None"}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a a course." />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="None" disabled>
+                          None
+                        </SelectItem>
+                        {courses.map((course) => (
+                          <SelectItem
+                            key={course.id}
+                            value={course.id}
+                            // TODO: Disable all courses user is in a study group for.
+                            disabled={false}
+                          >
+                            {course.title}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="users"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Users</FormLabel>
+                    <FormControl>
+                      <MultipleSelector
+                        {...field}
+                        defaultOptions={options}
+                        // TODO: Automatically select the current user
+                        placeholder={
+                          field.value?.length ?? ""
+                            ? ""
+                            : "Select users... (don't forget to add yourself!)"
+                        }
+                        emptyIndicator={<p>No Results Found.</p>}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
               {loading ? (
                 <Button disabled className="w-full">
                   <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
