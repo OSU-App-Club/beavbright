fbb263c22269e4c1d1134fb55726eacaa65e85fb
Cassius Villareal <cassiusfive@gmail.com>
2024-06-03 20:25:19 -0700

    fix: remove ability to create courses

--- apps/web/app/platform/study-groups/form.tsx
+++ apps/web/app/platform/study-groups/form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createStudyGroup, createCourse } from "@/app/lib/actions";
+import { createStudyGroup } from "@/app/lib/actions";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import { Button } from "@ui/components/ui/button";
@@ -34,113 +34,6 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
-
-export function JoinCourseForm() {
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const formSchema = z.object({
-    subject: z.string().min(2).max(6),
-    code: z.number().positive(),
-    title: z.string().min(1),
-  });
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-  });
-
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    setLoading(true);
-    try {
-      await createCourse(values);
-      setLoading(false);
-      toast.success("Course created successfully");
-      form.reset();
-    } catch (error) {
-      console.error(error);
-      setLoading(false);
-      toast.error("Failed to create course");
-    }
-  }
-
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline" className="text-primary">
-          <Plus className="mr-2" />
-          Add Course
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[450px]">
-        <DialogHeader>
-          <DialogTitle>Create Course</DialogTitle>
-          <DialogDescription>Add your course here.</DialogDescription>
-        </DialogHeader>
-        <div className="grid">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="subject"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Subject</FormLabel>
-                    <FormControl>
-                      <Input placeholder="ex. CS, MTH..." {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="code"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Code</FormLabel>
-                    <FormControl>
-                      <Input
-                        onChange={(e) => {
-                          field.onChange(parseInt(e.target.value));
-                        }}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="title"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Title</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="ex. Introduction to Computer Science"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {loading ? (
-                <Button disabled className="w-full">
-                  <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-                  Creating...
-                </Button>
-              ) : (
-                <Button type="submit" className="w-full">
-                  Add
-                </Button>
-              )}
-            </form>
-          </Form>
-        </div>
-      </DialogContent>
-    </Dialog>
-  );
-}
 
 export function CreateStudyGroupForm({
   users,
