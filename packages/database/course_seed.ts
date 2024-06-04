f40fc6aea750d0a85e8d5175fe14285ff231448a
Cassius Villareal <cassiusfive@gmail.com>
2024-06-03 19:27:56 -0700

    feat: cli spinner

--- packages/database/course_seed.ts
+++ packages/database/course_seed.ts
@@ -1,17 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 import scraper from "./scraper";
-
+import ora from "ora";
 const client = new PrismaClient();
 
-const courses = await scraper.getCourses("202403"); // Spring Term
-for (const course of courses) {
-  await client.course.upsert({
-    where: { title: course.title },
-    update: {},
-    create: {
-      title: course.title,
-      code: course.code,
-      subject: course.subject,
-    },
-  });
+async function main() {
+  const spinner = ora("Fetching courses... This might take a while.").start();
+
+  try {
+    const courses = await scraper.getCourses("202403"); // Spring Term
+    for (const course of courses) {
+      await client.course.upsert({
+        where: { title: course.title },
+        update: {},
+        create: {
+          title: course.title,
+          code: course.code,
+          subject: course.subject,
+        },
+      });
+    }
+    spinner.succeed("Course data successfully seeded");
+  } catch (error) {
+    spinner.fail("Failed to seed courses");
+    console.error(error);
+  }
 }
+
+main().finally(() => client.$disconnect());
