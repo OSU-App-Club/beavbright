6963f95e79187112bacdca4690406c3b9c510419
Cassius Villareal <cassiusfive@gmail.com>
2024-06-03 19:07:19 -0700

    feat: convert /scrape to script

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,7 +2,6 @@ import { serve } from "@hono/node-server";
 import { PrismaClient, User, Course } from "database";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import scraper from "./scraper";
 
 const client = new PrismaClient();
 const app = new Hono();
@@ -17,22 +16,6 @@ app.use(
 app.get("/users", async (c) => {
   const users = await client.user.findMany();
   return c.json(users);
-});
-
-app.post("/scrape", async (c) => {
-  const courses = await scraper.getCourses("202403");
-  for (const course of courses) {
-    await client.course.upsert({
-      where: { title: course.title },
-      update: {},
-      create: {
-        title: course.title,
-        code: course.code,
-        subject: course.subject,
-      },
-    });
-  }
-  return c.json({ message: "Scrape successful", ok: true });
 });
 
 serve({ fetch: app.fetch, port: 3001 });
