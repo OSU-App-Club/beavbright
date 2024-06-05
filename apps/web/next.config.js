f964f7088d3e239b50983e7a05e62f5f1f34f138
Thomas <nyumat18@gmail.com>
2024-06-05 15:03:21 -0700

    add placeholder image to next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,28 +1,34 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-	reactStrictMode: true,
-	transpilePackages: ["ui"],
-	output: "standalone",
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "0egpxzpbubygnrd0.public.blob.vercel-storage.com",
-				port: "",
-				pathname: "/**",
-			},
-			{
-				protocol: "https",
-				hostname: "via.placeholder.com",
-				port: "",
-				pathname: "/**",
-			},
-			{
-				protocol: "https",
-				hostname: "lh3.googleusercontent.com",
-				port: "",
-				pathname: "**",
-			},
-		],
-	},
+  reactStrictMode: true,
+  transpilePackages: ["ui"],
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "0egpxzpbubygnrd0.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "fakeimg.pl",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
