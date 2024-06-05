37c53045def2512f3579edf474d88344f50200df
Thomas <nyumat18@gmail.com>
2024-06-05 15:03:50 -0700

    utils: new getFileType utility methods

--- packages/ui/lib/utils.ts
+++ packages/ui/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getMimeType(base64: string): string | null {
+  const match = base64.match(/^data:(.*?);base64,/);
+  return match ? match[1] : null;
+}
+
+// TODO: Make this support more file types
+export function getVercelBlobFileType(link: string): string {
+  if (!link.endsWith(".pdf")) {
+    return `image/${link.split(".").pop()}`;
+  } else {
+    return "application/pdf";
+  }
+}
+
+export function isValidBody<T extends Record<string, unknown>>(
+  body: any,
+  fields: (keyof T)[]
+): body is T {
+  return Object.keys(body).every((key) => fields.includes(key));
+}
